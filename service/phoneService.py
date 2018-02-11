7a005291ab4981b12ac21a588bec7b9471679874
Hicham Dakhli <hicham.dakhli@axa.fr>
2018-02-11 22:13:32 +0100

    Solution to Exo01

--- service/phoneService.py
+++ service/phoneService.py
@@ -24,40 +24,40 @@ class PhoneService:
      return All premium phones
     '''
     def get_all_premium_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['cost'] >= 400, self._phones))
 
     '''
      return Only brand Samsung
     '''
     def get_samsung_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['brand'] == 'SAMSUNG', self._phones))
 
     '''
      return Only premium brand Samsung
     '''
     def get_premium_samsung_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['brand'] == 'SAMSUNG' and phone['cost'] >= 400, self._phones))
 
     '''
      return Either brand Samsung or HTC
     '''
     def get_samsung_and_htc_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['brand'] == 'SAMSUNG' or phone['brand'] == 'HTC', self._phones))
 
     '''
      return All premium and brand Samsung or HTC
     '''
     def get_premium_samsung_and_htc_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['cost'] >= 400 and (phone['brand'] == 'SAMSUNG' or phone['brand'] == 'HTC'), self._phones))
 
     '''
      return All but brand Samsung
     '''
     def get_all_except_samsung_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['brand'] != 'SAMSUNG', self._phones))
 
     '''
      return All premium but brand Samsung and htc
     '''
     def get_all_premium_except_samsung_and_htc_phones(self):
-        return self._phones
+        return list(filter(lambda phone: phone['cost'] >= 400 and (phone['brand'] != 'SAMSUNG' and phone['brand'] != 'HTC'), self._phones))
