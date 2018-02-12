298f45efc41382165200a0e14c3ae241eca56f4d
Hicham Dakhli <hicham.dakhli@axa.fr>
2018-02-12 21:08:32 +0100

    Solution to Exo04

--- specifications/premiumPhoneSpecification.py
+++ specifications/premiumPhoneSpecification.py
@@ -3,4 +3,4 @@ from specification_core.specification import Specification
 class PremiumPhoneSpecification(Specification):
 
     def is_satisfied_by(self, phone):
-        return phone['cost'] >= 400
+        return phone['cost'] >= 400 and phone['type'] != 'BASIC'
